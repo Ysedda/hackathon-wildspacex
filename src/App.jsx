7b48d2939c858d75bb7ce59b2c2387f258636e51
Ysedda <arthur.baudoin@icloud.com>
2022-11-24 12:45:03 +0100

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,11 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
-
   return (
-    <ChakraProvider>
-      <div>WildSpaceX</div>
-    </ChakraProvider>
+    <div>
+      <Home />
+    </div>
   );
 }
 
