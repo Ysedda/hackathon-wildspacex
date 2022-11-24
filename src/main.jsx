a41f44fc09ec5eeeca0f72993f9248a83780aa4a
MGliem <maximilian.gliem@gmail.com>
2022-11-24 19:58:12 +0100

    scale

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
@@ -6,18 +6,32 @@ import App from "./App";
 import "./index.css";
 import { extendTheme } from "@chakra-ui/react";
 
+const config = {
+  useSystemColorMode: false,
+  initialColorMode: "dark",
+}
+
 const theme = extendTheme({
+  config,
+  styles: {
+    global: (props) => ({
+      body: {
+        bg: "#000000",
+      },
+    }),
+  },
   fonts: {
-    heading: 'Righteous',
-    body: 'JetBrains Mono'
-  },});
+    heading: "Righteous",
+    body: "JetBrains Mono",
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
 );
