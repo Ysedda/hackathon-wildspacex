684e81ff5f4f34a08480d5300188683e2f3d12c0
kylian moreira <kylianmoreira@gmail.com>
2022-11-24 13:16:43 +0100

    add planets file

---
+++ planets.js
@@ -0,0 +1,343 @@
+export const planets = [
+  {
+    name: "Sun",
+    image: "sun.jpg",
+    activities: [
+      { name: "Bronzage", image: "bronzage.jpg" },
+      { name: "Barbecue", image: "barbecue.jpg" },
+      { name: "Source chaude", image: "source.jpg" },
+    ],
+    destinations: [
+      {
+        planet: "Mars",
+        date: "29/11/2022",
+      },
+      {
+        planet: "Venus",
+        date: "06/12/2022",
+      },
+      {
+        planet: "Terre",
+        date: "14/12/2022",
+      },
+      {
+        planet: "Uranus",
+        date: "04/12/2022",
+      },
+      {
+        planet: "Jupiter",
+        date: "05/01/2023",
+      },
+      {
+        planet: "Neptune",
+        date: "12/12/2022",
+      },
+      {
+        planet: "Mercure",
+        date: "28/11/2022",
+      },
+      {
+        planet: "Saturne",
+        date: "29/12/2022",
+      },
+    ],
+  },
+  {
+    name: "Mars",
+    image: "mars.jpg",
+    activities: [
+      { name: "Manger des snacks", image: "mars.jpg" },
+      { name: "Visite chutes d'eau", image: "eau.jpg" },
+      { name: "Olympus Mons", image: "montagne.jpg" },
+    ],
+    destinations: [
+      {
+        planet: "Sun",
+        date: "29/11/2022",
+      },
+      {
+        planet: "Venus",
+        date: "06/12/2022",
+      },
+      {
+        planet: "Terre",
+        date: "08/12/2022",
+      },
+      {
+        planet: "Uranus",
+        date: "09/12/2022",
+      },
+      {
+        planet: "Jupiter",
+        date: "25/11/2022",
+      },
+      {
+        planet: "Neptune",
+        date: "27/11/2022",
+      },
+      {
+        planet: "Mercure",
+        date: "30/11/2022",
+      },
+      {
+        planet: "Saturne",
+        date: "04/12/2022",
+      },
+    ],
+  },
+  {
+    name: "Terre",
+    image: "mars.jpg",
+    activities: [
+      { name: "Moncuq", image: "moncuq.jpg" },
+      { name: "Lab'O", image: "labo.jpg" },
+      { name: "Surf", image: "surf.jpg" },
+    ],
+    destinations: [
+      {
+        planet: "Mars",
+        date: "29/11/2022",
+      },
+      {
+        planet: "Venus",
+        date: "06/12/2022",
+      },
+      {
+        planet: "Sun",
+        date: "02/01/2023",
+      },
+      {
+        planet: "Uranus",
+        date: "14/12/2022",
+      },
+      {
+        planet: "Jupiter",
+        date: "30/11/2022",
+      },
+      {
+        planet: "Neptune",
+        date: "29/11/2022",
+      },
+      {
+        planet: "Mercure",
+        date: "29/11/2022",
+      },
+      {
+        planet: "Saturne",
+        date: "25/11/2022",
+      },
+    ],
+  },
+  {
+    name: "Venus",
+    image: "mars.jpg",
+    activities: [
+      { name: "Faire du tennis avec Mme Williams", image: "tennis.jpg" },
+      { name: "Usine Gillette", image: "gillette.jpg" },
+    ],
+    destinations: [
+      {
+        planet: "Mars",
+        date: "29/11/2022",
+      },
+      {
+        planet: "Sun",
+        date: "06/12/2022",
+      },
+      {
+        planet: "Terre",
+        date: "24/11/2022",
+      },
+      {
+        planet: "Uranus",
+        date: "26/11/2022",
+      },
+      {
+        planet: "Jupiter",
+        date: "25/12/2022",
+      },
+      {
+        planet: "Neptune",
+        date: "05/12/2022",
+      },
+      {
+        planet: "Mercure",
+        date: "29/11/2022",
+      },
+      {
+        planet: "Saturne",
+        date: "26/12/2022",
+      },
+    ],
+  },
+  {
+    name: "Jupiter",
+    image: "mars.jpg",
+    activities: [
+      { name: "Visite guidée avec M.Channing Tatum", image: "tatum.jpg" },
+      { name: "Chasseur de tornade", image: "tornade.jpg" },
+    ],
+    destinations: [
+      {
+        planet: "Mars",
+        date: "28/11/2022",
+      },
+      {
+        planet: "Venus",
+        date: "07/12/2022",
+      },
+      {
+        planet: "Terre",
+        date: "23/12/2022",
+      },
+      {
+        planet: "Uranus",
+        date: "08/12/2022",
+      },
+      {
+        planet: "Sun",
+        date: "29/12/2022",
+      },
+      {
+        planet: "Neptune",
+        date: "31/12/2022",
+      },
+      {
+        planet: "Mercure",
+        date: "01/01/2022",
+      },
+      {
+        planet: "Saturne",
+        date: "16/12/2022",
+      },
+    ],
+  },
+  {
+    name: "Saturne",
+    image: "mars.jpg",
+    activities: [
+      { name: "Kart sur les anneaux", image: "kart.jpg" },
+      { name: "Visite des lunes", image: "lune.jpg" },
+      { name: "Trou de ver de Nolan", image: "source.jpg" },
+    ],
+    destinations: [
+      {
+        planet: "Mars",
+        date: "28/12/2022",
+      },
+      {
+        planet: "Venus",
+        date: "06/12/2022",
+      },
+      {
+        planet: "Terre",
+        date: "07/12/2022",
+      },
+      {
+        planet: "Uranus",
+        date: "25/12/2022",
+      },
+      {
+        planet: "Jupiter",
+        date: "26/11/2022",
+      },
+      {
+        planet: "Neptune",
+        date: "28/11/2022",
+      },
+      {
+        planet: "Mercure",
+        date: "29/11/2022",
+      },
+      {
+        planet: "Sun",
+        date: "27/11/2022",
+      },
+    ],
+  },
+  {
+    name: "Uranus",
+    image: "mars.jpg",
+    activities: [
+      { name: "Blanchiment des ... dents", image: "dent.jpg" },
+      { name: "Musée Jeux de mots", image: "mots.jpg" },
+      { name: "Visite de ses lunes", image: "lunes.jpg" },
+    ],
+    destinations: [
+      {
+        planet: "Mars",
+        date: "29/11/2022",
+      },
+      {
+        planet: "Venus",
+        date: "06/12/2022",
+      },
+      {
+        planet: "Terre",
+        date: "05/12/2022",
+      },
+      {
+        planet: "Sun",
+        date: "06/01/2022",
+      },
+      {
+        planet: "Jupiter",
+        date: "04/12/2022",
+      },
+      {
+        planet: "Neptune",
+        date: "28/11/2022",
+      },
+      {
+        planet: "Mercure",
+        date: "11/01/2023",
+      },
+      {
+        planet: "Saturne",
+        date: "28/11/2022",
+      },
+    ],
+  },
+  {
+    name: "Neptune",
+    image: "mars.jpg",
+    activities: [
+      { name: "Boire un triton sur Triton", image: "triton.jpg" },
+      { name: "Viste du cortege", image: "cortege.jpg" },
+    ],
+    destinations: [
+      {
+        planet: "Mars",
+        date: "29/11/2022",
+      },
+      {
+        planet: "Venus",
+        date: "06/12/2022",
+      },
+      {
+        planet: "Terre",
+        date: "05/12/2022",
+      },
+      {
+        planet: "Uranus",
+        date: "30/11/2022",
+      },
+      {
+        planet: "Jupiter",
+        date: "12/01/2023",
+      },
+      {
+        planet: "Sun",
+        date: "05/12/2022",
+      },
+      {
+        planet: "Mercure",
+        date: "30/01/2023",
+      },
+      {
+        planet: "Saturne",
+        date: "29/11/2022",
+      },
+    ],
+  },
+];
