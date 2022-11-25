1fd3a2ffc4d909f736e28819355442fe3b195ee0
Ysedda <arthur.baudoin@icloud.com>
2022-11-25 13:11:28 +0100

    alignment

--- src/components/citation/Citation.jsx
+++ src/components/citation/Citation.jsx
@@ -1,4 +1,5 @@
-import { Box, Card, CardBody, CardFooter, Divider, Heading, Image, Stack, Text } from '@chakra-ui/react'
+import { Box, Card, CardBody, CardFooter, Divider, Heading, Image, Stack, Text, Flex } from '@chakra-ui/react'
+import { AiFillStar } from "react-icons/ai";
 
 const Citation = () => {
   return (
@@ -24,8 +25,10 @@ const Citation = () => {
             >
               Un superbe moment de partage, j'avais jamais vu un trou d'aussi prêt. Il manque peut-être d'animation autour de ce trou, j'aurais aimé mettre des choses dedans.
             </Text>
-            <Text color='yellow.600' fontSize='2xl'>
-              * * * *
+            <Text color='yellow.600' fontSize='3xl'>
+              <Flex>
+             <AiFillStar /> <AiFillStar /> <AiFillStar /> <AiFillStar />
+             </Flex>
             </Text>
           </Stack>
         </CardBody>
@@ -47,8 +50,10 @@ const Citation = () => {
             <Text color='white'>
               Tout simplement une anarnaque!!! On m'a vendu un bronzage au top... je suis revenu sans peau, parce qu'on ne mentionne pas l'indice UV avant l'exposition avec le Soleil! Une honte
             </Text>
-            <Text color='yellow.600' fontSize='2xl'>
-              *
+            <Text color='yellow.600' fontSize='3xl'>
+            <Flex>
+             <AiFillStar />
+             </Flex>
             </Text>
           </Stack>
         </CardBody>
@@ -64,14 +69,16 @@ const Citation = () => {
             backgroundPosition={'cover'}
             height={'200px'}
           />
-          <Stack mt='6' spacing='3'>
+          <Stack mt='6' spacing='3' >
             <Divider />
             <Heading size='md' color='yellow.600'>Uranus / 06/10/2022</Heading>
-            <Text color='white'>
+            <Text color='white' h='144px'>
               Plus de 30 minutes pour faire Terre - Uranus!! Hereusement j'ai pu me faire blanchir rapidement pour mon date du soir! La prochaine fois donner des coupons de reduction! 
             </Text>
-            <Text color='yellow.600' fontSize='2xl'>
-              * *
+            <Text color='yellow.600' fontSize='3xl'>
+            <Flex>
+             <AiFillStar /><AiFillStar />
+             </Flex>
             </Text>
           </Stack>
         </CardBody>
