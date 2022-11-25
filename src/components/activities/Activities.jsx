1506d93cc373b7316f7220b791c713ddb309ea48
Paul Chatelain-Fernandes <paulchatelainfernandes@gmail.com>
2022-11-25 10:54:34 +0100

    added style to activities modal

--- src/components/activities/Activities.jsx
+++ src/components/activities/Activities.jsx
@@ -11,34 +11,35 @@ import {
   Box,
   Heading,
   Select,
-  Text
+  Text,
 } from "@chakra-ui/react";
 import { planets } from "../../assets/planets";
 
 const Activities = ({ planetName }) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   const myPlanet = planets.filter((planet) => planet.name === planetName);
-  const myPlanetActivities = {...myPlanet[0].activities}
-  
+  const myPlanetActivities = { ...myPlanet[0].activities };
+
   return (
     <>
       <Button onClick={() => onOpen()} key="full" m={4} zIndex="3">
         Voir les activités
       </Button>
+
       <Modal onClose={onClose} size="full" isOpen={isOpen}>
         <ModalOverlay />
-        <ModalContent>
-          <ModalHeader m="2rem auto auto auto">
+        <ModalContent bgColor="#111">
+          <ModalHeader m="2rem auto auto auto" color="#fff">
             Activités sur {planetName}
           </ModalHeader>
           <ModalCloseButton m="2rem 2rem auto auto" />
-          <ModalBody>
-          {myPlanet &&
-                  Object.values(myPlanetActivities).map((activity, index) => (
-                    <Text key={index} color="black">
-                      {activity.name}
-                    </Text>
-                  ))}
+          <ModalBody color="#fff">
+            {myPlanet &&
+              Object.values(myPlanetActivities).map((activity, index) => (
+                <Text key={index} color="#fff">
+                  {activity.name}
+                </Text>
+              ))}
           </ModalBody>
           <ModalFooter>
             <Button onClick={onClose}>Retour au système solaire</Button>
